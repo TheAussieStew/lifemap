29c101aee0c34fb93e721aeedf00726da9a5ffd7
TheAussieStew <yingkongwei@gmail.com>
2020-10-31 16:50:27 +1100

    Actually get react-force-graph working

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { ForceGraph2D } from "react-force-graph";
+import data from "./miserables.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ForceGraph2D
+      graphData={data}
+      nodeLabel="id"
+      nodeAutoColorBy="group"
+      linkDirectionalParticles="value"
+      linkDirectionalParticleSpeed={0.001}
+    />
   );
 }
 
