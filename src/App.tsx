afb1d16a13303868c8f119f7c5f16199909ef633
TheAussieStew <yingkongwei@gmail.com>
2020-11-02 19:38:35 +1100

    Attempt to get material popover going

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,70 @@
 import React from "react";
 import "./App.css";
 import { ForceGraph2D } from "react-force-graph";
+import { makeStyles } from "@material-ui/core/styles";
+import Popover from "@material-ui/core/Popover";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 import data from "./kongweilifemap.json";
+
+const useStyles = makeStyles((theme) => ({
+  typography: {
+    padding: theme.spacing(2),
+  },
+}));
+
+const classes = useStyles();
+const [anchorEl, setAnchorEl] = React.useState(null);
+
+const handleClick = (event: any) => {
+  setAnchorEl(event.currentTarget);
+};
+
+const handleClose = () => {
+  setAnchorEl(null);
+};
+
+const open = Boolean(anchorEl);
+const id = open ? "simple-popover" : undefined;
 
 function App() {
   return (
-    <ForceGraph2D
-      graphData={data}
-      nodeLabel="id"
-      nodeAutoColorBy="group"
-      linkDirectionalParticles="value"
-      linkDirectionalParticleSpeed={0.01}
-    />
+    <div>
+      <Button
+        aria-describedby={id}
+        variant="contained"
+        color="primary"
+        onClick={handleClick}
+      >
+        Open Popover
+      </Button>
+      <ForceGraph2D
+        graphData={data}
+        nodeLabel="id"
+        nodeAutoColorBy="group"
+        linkDirectionalParticles="value"
+        linkDirectionalParticleSpeed={0.01}
+        linkDirectionalParticleWidth={5}
+      />
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <Typography className={classes.typography}>
+          The content of the Popover.
+        </Typography>
+      </Popover>
+    </div>
   );
 }
 
