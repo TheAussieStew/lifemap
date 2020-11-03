c6bba5ebdbb14a87e02eac841eb76c4049c40b92
TheAussieStew <yingkongwei@gmail.com>
2020-11-03 18:30:37 +1100

    Get dynamic data changes working

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,64 @@ import "./App.css";
 import { ForceGraph2D } from "react-force-graph";
 import data from "./kongweilifemap.json";
 import Popover from "@material-ui/core/Popover";
-import TextField from '@material-ui/core/TextField';
+import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 
+type NodeObject$3 = object & {
+  id?: string | number;
+  x?: number;
+  y?: number;
+  vx?: number;
+  vy?: number;
+  fx?: number;
+  fy?: number;
+};
 
 const App = () => {
-
-  const useStyles = makeStyles((theme) => ({
-    typography: {
-      padding: theme.spacing(2),
-    },
-  }));
-
-  const classes = useStyles();
   let initialAnchor: number = 0;
-
+  let initialNode: NodeObject$3 = {
+    id: 0,
+    x: 0,
+    y: 0,
+    vx: 0,
+    vy: 0,
+    fx: 0,
+    fy: 0
+  };
   const [anchorX, setAnchorX] = React.useState(initialAnchor);
   const [anchorY, setAnchorY] = React.useState(initialAnchor);
+  const [selectedNode, setSelectedNode] = React.useState(initialNode);
+  const [textValue, setTextValue] = React.useState("");
 
-  const handleClick = (node: any, event: MouseEvent) => {
+  const updatedData = data;
+
+  const updateId = (oldId: string, newId: string) => {
+    for (var i = 0; i < updatedData.nodes.length; i++) {
+      if (updatedData.nodes[i].id === oldId) {
+        updatedData.nodes[i].id = newId;
+        return;
+      }
+    }
+  };
+
+  const handleClick = (node: NodeObject$3, event: MouseEvent) => {
     setAnchorX(event.x);
     setAnchorY(event.y);
+    setTextValue(node.id ? node.id.toString() : "");
+    setSelectedNode(node);
   };
 
   const handleClose = () => {
     setAnchorX(0);
     setAnchorY(0);
+  };
+
+  const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTextValue(event.target.value);
+    updateId(
+      selectedNode.id ? selectedNode.id.toString() : "",
+      event.target.value
+    );
   };
 
   const open = Boolean(anchorX && anchorY);
@@ -56,13 +88,14 @@ const App = () => {
           label="Details"
           multiline
           rows={6}
-          defaultValue="Default Value"
           variant="outlined"
+          onChange={handleTextChange}
           style={{ margin: 24 }}
+          value={textValue}
         />
       </Popover>
       <ForceGraph2D
-        graphData={data}
+        graphData={updatedData}
         nodeLabel="id"
         nodeAutoColorBy="group"
         linkDirectionalParticles="value"
@@ -72,6 +105,6 @@ const App = () => {
       />
     </div>
   );
-}
+};
 
 export default App;
