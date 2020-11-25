46abad651dea271ca461b510a6cd4caa4ea3cb6f
TheAussieStew <yingkongwei@gmail.com>
2020-11-25 18:44:48 +1100

    Refine model

--- src/components/LifeGraphModel.tsx
+++ src/components/LifeGraphModel.tsx
@@ -1,28 +1,55 @@
-export type Pattern = Date | number | string | Recurring;
-export type Recurring = unknown;
+// Enforce ES6 arrow syntax. Enforce return arguments in fn defs
+// TODO: prettier, eslint
+import { DateTime, Interval, Duration } from 'luxon';
 
-export type PartialOrder<OrderKind> = OrderKind & OrderKind;
+export type PartialOrder<OrderKind> = {
+    from: OrderKind;
+    to: OrderKind;
+    comparator: Comparator;
+}
+export type Comparator = (from: OrderMetric, to: OrderMetric) => number;
 export type Unordered = undefined;
 
-export type OrderKind = QiZhi | Ratio | List | TimeHorizon;
+export type Pattern = Time | number | string | Recurring;
+export type Recurring = unknown;
+
+export type OrderMetric = QiZhi | Ratio | List | TimePoint;
 export type QiZhi = number;
 export type Ratio = number;
-export type List = boolean; // how to enforce that one has to be true, one false?
-export type TimeHorizon = Date; // how to enforce that one has to be true, one false?
+export type List = boolean;
+
+export type Time = TimePoint | TimeDuration | TimeSpan;
+export type TimePoint = DateTime;
+export type TimeDuration = Duration;
+export type TimeSpan = Interval;
+
+export type GraphData = {
+    nodes: Qis;
+    links: QiLinks;
+}
+
+export type Qis = {
+    [id: number]: Qi;
+}
+
+export type QiLinks = {
+    [id: number]: QiLink<OrderMetric>;
+}
 
 export type Qi = {
-    id: number; // number lookup is faster than string
+    readonly id: number; // number lookup is faster than string
     mutability: boolean;
     pattern: Pattern;
     transformable: boolean;
     qiQuality: QiZhi;
-    timeHorizon: qiLink<TimeHorizon>;
+    pulsation: (qiQuality: QiZhi) => number // field/gravity strength;
+    timeHorizon: QiLink<TimeSpan>;
 };
 
-export type qiLink<OrderKind> = {
-    id: number;
-    from: Qi;
-    to: Qi;
+export type QiLink<OrderKind> = {
+    readonly id: number;
+    readonly from: Qi;
+    readonly to: Qi;
     tong: QiZhi;
     ordering: Unordered | PartialOrder<OrderKind>;
 };
