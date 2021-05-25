5a933c03cda49a3a78c89f5f81456c7aaa180b4d
TheAussieStew <yingkongwei@gmail.com>
2021-05-25 13:35:33 +1000

    Get basic flow + text editor example working

--- src/core/Tiptap.tsx
+++ src/core/Tiptap.tsx
@@ -10,8 +10,8 @@ const Tiptap: React.FC<{content: string}> = ({content}) => {
     content: content,
     onUpdate({editor}) {
       const json = editor.getJSON()
-      console.log("sd", json)
-      console.log("sd", json.content[0].content[0].text)
+      // console.log("sd", json)
+      // console.log("sd", json.content[0].content[0].text)
       // send the content to an API here
     }
   })
