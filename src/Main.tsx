3f8ecf7ede10283b17a04780f186f479975dda64
TheAussieStew <yingkongwei@gmail.com>
2021-09-11 17:38:18 +1000

    Remove unused testing

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { ShenT, GraphCorrect } from "./core/LifeGraphModel";
-import { GraphTest } from "./utils/Testing";
 import { TreeCorrect, Graph3DCorrect, Graph2DTipTap, Graph2DReactForce, LoggingCorrect } from "./view/View";
 
 export const GraphContext = React.createContext<ShenT>(GraphCorrect.createShen());
