3dc38d363b6856d28283f15c94e62452f5563c63
TheAussieStew <yingkongwei@gmail.com>
2021-11-05 09:38:52 +1100

    Change to use store

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,29 +1,13 @@
 import React from "react";
-import { ShenT, GraphCorrect } from "./core/LifeGraphModel";
-import { TreeCorrect, Graph3DCorrect, Graph2DTipTap, Graph2DReactForce, LoggingCorrect } from "./view/View";
-
-export const GraphContext = React.createContext<ShenT>(GraphCorrect.createShen());
+import { ShenContext, Store } from "./core/Store";
+import { Bubble } from "./view/Bubble";
 
 const Main = () => {
-  const shen = GraphCorrect.createShen();
-  shen.information = "The Void";
-  let { q, s1 } = GraphCorrect.createRelation(shen);
-  q.information = "Hello universe!";
   return (
-    <GraphContext.Provider value={shen}>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: `repeat(auto-fit, minmax(250px, 1fr))`,
-        }}
-      >
-        <Graph3DCorrect />
-        <Graph2DTipTap />
-        <LoggingCorrect />
-        {/* <Graph2DReactForce /> */}
-      </div>
-    </GraphContext.Provider>
-  );
+    <Store>
+      <Bubble q={React.useContext(ShenContext)} />
+    </Store>
+  )
 };
 
 export default Main;
