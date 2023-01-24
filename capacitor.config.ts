2abca5f183bcf17bed48cc809551b2928dca6229
conwayying <101533804+conwayying@users.noreply.github.com>
2023-01-24 16:14:25 +1100

    Configure Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.eusaybia.app.lifemap',
+  appName: 'lifemap',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
