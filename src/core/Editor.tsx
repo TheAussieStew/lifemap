7c61d57c121afb4a5c9098490683fa6a42ac6eaa
conwayying <101533804+conwayying@users.noreply.github.com>
2023-01-30 21:09:18 +1100

    Successfully get group extension working

--- src/core/Editor.tsx
+++ src/core/Editor.tsx
@@ -4,22 +4,43 @@ import { IndexeddbPersistence } from 'y-indexeddb'
 import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import React from 'react'
+import { GroupExtension } from '../view/structure/GroupTipTapExtension'
+import { generateUniqueID } from '../utils/utils'
+import { MathExtension } from '../view/content/MathTipTapExtension'
 
-const ydoc = new Y.Doc()
 
-// Store the Y document in the browser
-new IndexeddbPersistence('example-document', ydoc)
 
 const Editor = () => {
+  const ydoc = new Y.Doc()
+  // A unique ID per document
+  const yDocumentName = generateUniqueID()
+  console.debug(yDocumentName)
+  // Store the Y document in the browser
+  new IndexeddbPersistence(yDocumentName, ydoc)
+
   const editor = useEditor({
     extensions: [
       // @ts-ignore
-      StarterKit,
+      StarterKit.configure({
+        history: false
+      }),
       Collaboration.configure({
         document: ydoc,
       }),
+      GroupExtension,
+      MathExtension
     ],
+    onUpdate: ({ editor }) => {
+      console.debug("JSON Output", editor.getJSON())
+      console.debug("HTML Output", editor.getHTML())
+    },
   })
+
+  if (process.env.NODE_ENV === 'development') {
+    if (editor) {
+      // console.debug(editor.schema)
+    }
+  }
 
   return (
     <EditorContent editor={editor} />
