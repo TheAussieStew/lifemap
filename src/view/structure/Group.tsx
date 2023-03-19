4595e39d3026fbe7accd3e74df3cfea0bec291da
conwayying <101533804+conwayying@users.noreply.github.com>
2023-03-19 12:44:18 +1100

    Add animations to group

--- src/view/structure/Group.tsx
+++ src/view/structure/Group.tsx
@@ -1,26 +1,46 @@
 import React from 'react'
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import { Qi } from '../../core/Qi';
 
 export type GroupLenses = "verticalArray";
 
-export const Group = (props: {children: any, lens: GroupLenses}) => {
+export const Group = (props: { children: any, lens: GroupLenses }) => {
 
+    // TODO: Exit doesn't work
+    // TODO: Fix stretchy border: https://github.com/framer/motion/issues/1249
     return (
-        <motion.div
-            layoutId="group"
-            className="group"
-            style={{
-                minHeight: 20,
-                borderRadius: `10px`,
-                border: `2px solid var(--Light_Grey, #dddddd)`,
-                boxShadow: `0px 0.6032302072222955px 0.6032302072222955px -1.25px rgba(0, 0, 0, 0.18), 0px 2.290210571630906px 2.290210571630906px -2.5px rgba(0, 0, 0, 0.15887), 0px 10px 10px -3.75px rgba(0, 0, 0, 0.0625)`,
-                padding: `30px`,
-                // margin: `10px`,
-            }}
-        >
-            {props.children}
-        </motion.div>
+        <AnimatePresence>
+            <motion.div
+                key="group"
+                layoutId="group"
+                className="group"
+                initial={{
+                    scale: 0,
+                    opacity: 0,
+                    originX: 0,
+                    originY: 0
+                }}
+                animate={{ scale: 1, opacity: 1 }}
+                exit={{
+                    scale: 0,
+                    opacity: 0,
+                }}
+                transition={{
+                    type: "ease",
+                    duration: 0.3
+                }}
+                style={{
+                    minHeight: 20,
+                    borderRadius: `10px`,
+                    border: `2px solid var(--Light_Grey, #dddddd)`,
+                    boxShadow: `0px 0.6032302072222955px 0.6032302072222955px -1.25px rgba(0, 0, 0, 0.18), 0px 2.290210571630906px 2.290210571630906px -2.5px rgba(0, 0, 0, 0.15887), 0px 10px 10px -3.75px rgba(0, 0, 0, 0.0625)`,
+                    padding: `30px`,
+                    // margin: `10px`,
+                }}
+            >
+                {props.children}
+            </motion.div>
+        </AnimatePresence>
     )
 }
 
