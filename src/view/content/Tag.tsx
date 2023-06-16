4fa69fb1dd1b1d3e055d6a12187a78b67d8966e4
conwayying <101533804+conwayying@users.noreply.github.com>
2023-06-16 14:41:11 +1000

    Tweak styling of menu

--- src/view/content/Tag.tsx
+++ src/view/content/Tag.tsx
@@ -9,12 +9,12 @@ export const Tag = (props: { children: any }) => {
                 {
                     boxSizing: "border-box",
                     width: "fit-content",
-                    height: "27px",
+                    height: "fit-content",
                     display: "flex",
                     flexDirection: "row",
                     justifyContent: "center",
                     alignItems: "center",
-                    padding: "3px 10px 3px 10px",
+                    padding: "3px 20px 3px 20px",
                     boxShadow:
                       "0px 0.6021873017743928px 3.010936508871964px -0.9166666666666666px rgba(0, 0, 0, 0.14), 0px 2.288533303243457px 11.442666516217285px -1.8333333333333333px rgba(0, 0, 0, 0.13178), 0px 10px 50px -2.75px rgba(0, 0, 0, 0.1125)",
                     backgroundColor: "#ffffff",
