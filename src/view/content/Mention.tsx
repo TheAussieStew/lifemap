19e04de50751c8d373eb997a74514f37aa967539
conwayying <101533804+conwayying@users.noreply.github.com>
2023-07-05 17:47:41 +1000

    Fix bugs around tag handling

---
+++ src/view/content/Mention.tsx
@@ -0,0 +1,11 @@
+import { Mention, MentionOptions } from '@tiptap/extension-mention';
+import { Node as ProsemirrorNode } from 'prosemirror-model';
+
+export const CustomMention = Mention.extend({
+  addOptions(): MentionOptions {
+    return {
+      ...this.parent?.(),
+      renderLabel: ({ node }: { node: ProsemirrorNode }) => node.attrs.label,
+    };
+  },
+});
