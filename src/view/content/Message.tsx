1112cf4c1c8d8c2aae60364fabc4607b2dcf4a71
conwayying <101533804+conwayying@users.noreply.github.com>
2023-07-16 15:10:32 +1000

    Successfully add message adding functionality

---
+++ src/view/content/Message.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+interface MessageProps {
+  children: any;
+}
+
+interface ParsedMessage {
+  initials: string;
+  text: string;
+}
+
+export const parseMessage = (message: string): ParsedMessage => {
+  const [initials, text] = message.split(':');
+  return { initials: initials.trim(), text: text.trim() };
+};
+
+export const Message: React.FC<MessageProps> = (props: { children: any }) => {
+
+  const bubbleStyles: React.CSSProperties = {
+    position: 'relative',
+    padding: '10px',
+    borderRadius: '10px',
+    marginBottom: '10px',
+    minHeight: '20px',
+    maxWidth: '600px',
+    minWidth: '100px',
+    color: "#111111"
+  };
+
+  // TODO: Make it such that it uses a custom colour for each person. Not this self vs other format.
+
+  bubbleStyles.backgroundColor = '#3D5AFE';
+  bubbleStyles.color = '#fff';
+  bubbleStyles.alignSelf = 'flex-end';
+
+  return (
+    <div style={bubbleStyles}>
+      {"💬 "}
+      {props.children}
+    </div>
+  );
+};
---
+++ src/view/content/MessageExtension.tsx
@@ -0,0 +1,128 @@
+import { Editor, Node, findParentNode, mergeAttributes, nodeInputRule, wrappingInputRule } from '@tiptap/core';
+import { ReactNodeViewRenderer, NodeViewProps, NodeViewWrapper, NodeViewContent } from '@tiptap/react';
+import { Message } from './Message';
+import { Plugin, PluginKey, TextSelection } from 'prosemirror-state';
+import React from 'react';
+
+export const MessageExtension = Node.create({
+    name: 'message',
+    group: 'block',
+    content: 'inline*',
+    selectable: true,
+    parseHTML() {
+        return [
+            {
+                tag: 'message',
+            },
+        ];
+    },
+    renderHTML({ HTMLAttributes }) {
+        return ['message', mergeAttributes(HTMLAttributes), 0];
+    },
+    addInputRules() {
+        return [
+            // TODO: make the Regex rule, "" which automatically converts to a single chat message
+            // Whoever, creates the message will automatically add a avatar assigning the message to them
+            nodeInputRule({
+                find: /''([^'']*)''/,
+                type: this.type,
+                getAttributes: (match) => {
+                    console.log(match)
+                    return {
+                        textContent: match[1]
+                    };
+                }
+            }),
+        ];
+    },
+    addKeyboardShortcuts() {
+        return {
+            Enter: ({ editor }) => {
+                const { selection } = editor.state;
+                const messageNode = findParentNode(node => node.type.name === 'message')(selection);
+    
+                if (messageNode) {
+                    // Insert a mention tag at the current cursor position
+                    editor.commands.insertContent({
+                        type: 'mention',
+                        attrs: {
+                            id: 'mention-id',
+                            label: '↪️ needs reply'
+                        }
+                    });
+                    // Move the cursor to the position after the message node
+                    let newPosition = selection.$from.after(messageNode.depth) + 1;
+                    editor.commands.setTextSelection(newPosition)
+
+                    return true;
+                }
+    
+                return false;
+            }
+        }
+    },
+    // addKeyboardShortcuts() {
+    //     return {
+    //         Enter: ({ editor }) => {
+    //             const { selection } = editor.state;
+    //             const messageNode = selection.$from.node(1);
+    //             console.log("mn", messageNode)
+
+    //             if (messageNode && messageNode.type.name === 'message') {
+    //                 // Insert a new mention node at the end of the message node
+    //                 const endPosition = selection.$from.end(2);
+    //                 editor.commands.insertContentAt(endPosition, {
+    //                     type: 'mention',
+    //                     attrs: {
+    //                         id: 'mention-id',
+    //                         label: 'needs reply'
+    //                     }
+    //                 });
+
+    //                 // Move the cursor to the position after the message node
+    //                 // TODO: This is kinda hacky, see if I can get after the message node without using magic variables like newPos + 2
+    //                 const newPosition = selection.$from.after(2);
+    //                 editor.commands.setTextSelection(newPosition + 2);
+    //                 return true;
+    //                }
+
+
+    //             return false;
+    //         }
+    //     }
+    // },
+    addProseMirrorPlugins() {
+        return [messageCursorPlugin];
+    },
+    addNodeView() {
+        return ReactNodeViewRenderer((props: NodeViewProps) => {
+            return <NodeViewWrapper>
+                <Message>
+                    <NodeViewContent/>
+                </Message>
+            </NodeViewWrapper>
+        });
+    },
+});
+
+
+const messageCursorPlugin = new Plugin({
+    key: new PluginKey('messageCursor'),
+    view: () => ({
+        update: (view) => {
+            const { state } = view;
+            const { selection } = state;
+            const messageNode = state.doc.nodeAt(selection.from);
+
+            if (messageNode && messageNode.type.name === 'message') {
+                // Set the cursor position to the end of the message node
+                const endPosition = selection.from + messageNode.nodeSize - 1;
+                view.dispatch(
+                    state.tr.setSelection(
+                        TextSelection.create(state.doc, endPosition)
+                    )
+                );
+            }
+        },
+    }),
+});
