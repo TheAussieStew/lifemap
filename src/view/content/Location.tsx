3a2afbc4e1c46aed257d773aac6d150cd7c5d96a
conwayying <101533804+conwayying@users.noreply.github.com>
2023-07-18 14:06:55 +1000

    Add basic Mapbox example

---
+++ src/view/content/Location.tsx
@@ -0,0 +1,45 @@
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import 'styles.scss';
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidGhlYXVzc2llc3RldyIsImEiOiJjbGd1ZW1qaHowZmZsM3NudWdvYTY0c255In0.T7PzJ-D4ifBUDtbnRNbXFA';
+
+interface LocationProps { }
+
+export const Location: React.FC<LocationProps> = () => {
+    const mapContainer = useRef<HTMLDivElement>(null);
+    const map = useRef<mapboxgl.Map | null>(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
+
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current as HTMLElement,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [lng, lat],
+            zoom: zoom
+        });
+    });
+
+    useEffect(() => {
+        if (map.current) return; // wait for map to initialize
+        map.current!.on('move', () => {
+            setLng(Number(map.current!.getCenter().lng.toFixed(4)));
+            setLat(Number(map.current!.getCenter().lat.toFixed(4)));
+            setZoom(Number(map.current!.getZoom().toFixed(2)));
+        });
+    });
+
+    return (
+        <div>
+            <div className="sidebar">
+                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
+            <div ref={mapContainer} className="map-container" />
+        </div>
+    );
+}
