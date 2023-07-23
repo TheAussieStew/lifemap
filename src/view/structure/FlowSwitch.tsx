ddc4b14759358f1bf66f4d68f9829ebc645f0f25
conwayying <101533804+conwayying@users.noreply.github.com>
2023-07-23 20:06:35 +1000

    Linting

--- src/view/structure/FlowSwitch.tsx
+++ src/view/structure/FlowSwitch.tsx
@@ -1,5 +1,5 @@
-import { motion, useScroll, useVelocity } from "framer-motion"
-import React, { useRef } from "react"
+import { motion } from "framer-motion"
+import React from "react"
 import './styles.scss'
 
 export const FlowSwitch = (props: { children: React.ReactElement[], onChange?: (selectedIndex: number) => void, isLens?: boolean }) => {
