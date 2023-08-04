29c598e64fe0c88e85917857ef35e8317e60cdf1
conwayying <101533804+conwayying@users.noreply.github.com>
2023-08-04 13:35:10 +1000

    Get Next.js project working as per prior to transition

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+'use client'
 
-export default function Page() {
-    return <h1>Hello, Next.js!</h1>
-  }
+import { motion } from 'framer-motion';
+import * as React from 'react';
+import '@fontsource/public-sans';
+import { Qi } from '../src/core/Qi';
+
+const App = () => {
+  return (
+    <React.StrictMode>
+      <motion.div style={{ margin: 10, padding: `10px 30px 0px 30px` }}>
+          <Qi qiId={'000000'} userId={'000000'} />
+      </motion.div>
+    </React.StrictMode>
+  );
+};
+
+export default App;
