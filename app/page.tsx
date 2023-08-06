46314be93c1c7c104317bac6553c192ef1e0be61
conwayying <101533804+conwayying@users.noreply.github.com>
2023-08-06 22:33:05 +1000

    Update padding properly

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { Qi } from '../src/core/Qi';
 const App = () => {
   return (
     <React.StrictMode>
-      <motion.div style={{ margin: 10, padding: `10px 30px 0px 30px` }}>
+      <motion.div style={{ margin: 10, padding: `30px 30px 30px 30px` }}>
           <Qi qiId={'000000'} userId={'000000'} />
       </motion.div>
     </React.StrictMode>
