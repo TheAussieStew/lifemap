e7d4d6e5e426eaf9da95e3540d0a45fb7f042a37
conwayying <101533804+conwayying@users.noreply.github.com>
2023-08-06 17:01:59 +1000

    Prevent page generation on components that require access to client side DOM/interfaces

--- src/core/Qi.tsx
+++ src/core/Qi.tsx
@@ -2,18 +2,22 @@ import React from "react";
 import { QiId, Loupe, MathsLoupeC } from "../core/Model";
 import { QiStore, QiStoreContext } from "../backend/QiStore";
 import { QiView } from "../view/QiView";
+import NoSSR from "../utils/NoSSR";
 
 export const Qi = (props: { qiId: QiId; userId: string, loupe?: Loupe }) => {
   return (
     // This store is not visual, it is purely to feed information
     // @ts-ignore
-    <QiStore qiId={props.qiId} userId={props.userId}>
-      {/* Use the qi in QiStore 
+    <NoSSR>
+
+      <QiStore qiId={props.qiId} userId={props.userId}>
+        {/* Use the qi in QiStore 
       This seemingly gets the wrong qi, the default value, rather than
       the store provided one
       */}
-      <QiView qi={React.useContext(QiStoreContext)} />
-    </QiStore>
+        <QiView qi={React.useContext(QiStoreContext)} />
+      </QiStore>
+    </NoSSR>
   );
 };
 
