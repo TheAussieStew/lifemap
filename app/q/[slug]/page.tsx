6376dcdb24b34cde13d474b93f21fdb4432a2877
conwayying <101533804+conwayying@users.noreply.github.com>
2023-08-06 22:33:05 +1000

    Update padding properly

--- app/q/[slug]/page.tsx
+++ app/q/[slug]/page.tsx
@@ -6,7 +6,7 @@ import { Qi } from "../../../src/core/Qi";
 
 export default function Page({ params }: { params: { slug: string } }) {
     return (
-        <motion.div style={{ margin: 10, padding: `10px 30px 0px 30px` }}>
+        <motion.div style={{ margin: 10, padding: `30px 30px 30px 30px` }}>
           <Qi qiId={params.slug} userId={'000000'} />
       </motion.div>
     )
