346a4316db5b009191d4407b3a709a144c755f4c
conwayying <101533804+conwayying@users.noreply.github.com>
2023-08-08 21:21:32 +1000

    Add new mapping functionality

--- src/view/content/Location.tsx
+++ src/view/content/Location.tsx
@@ -1,6 +1,7 @@
 import './styles.scss';
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
+import Map from 'react-map-gl';
 import { SearchBox } from '@mapbox/search-js-react';
 
 export const mapboxAccessToken = 'pk.eyJ1IjoidGhlYXVzc2llc3RldyIsImEiOiJjbGd1ZW1qaHowZmZsM3NudWdvYTY0c255In0.T7PzJ-D4ifBUDtbnRNbXFA';
@@ -12,49 +13,22 @@ interface LocationProps {
 }
 
 export const Location: React.FC<LocationProps> = (props: LocationProps) => {
-    const mapContainer = useRef<HTMLDivElement>(null);
-    const map = useRef<mapboxgl.Map | null>(null);
     const [lng, setLng] = useState(151.21512);
     const [lat, setLat] = useState(-33.861344);
     const [zoom, setZoom] = useState(9);
 
-    useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current as HTMLElement,
-            center: [lng, lat],
-            pitch: 45,
-            // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-            style: 'mapbox://styles/mapbox/streets-v12',
-            zoom: zoom
-        });
-        map.current!.on('move', () => {
-            setLng(Number(map.current!.getCenter().lng.toFixed(4)));
-            setLat(Number(map.current!.getCenter().lat.toFixed(4)));
-            setZoom(Number(map.current!.getZoom().toFixed(2)));
-        });
-        map.current!.on('style.load', () => {
-            map.current!.addSource('mapbox-dem', {
-                'type': 'raster-dem',
-                'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
-                'tileSize': 512,
-                'maxzoom': 9 
-            });
-            // add the DEM source as a terrain layer with exaggerated height
-            map.current!.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
-        });
-        let lngLat = new mapboxgl.LngLat(lng, lat)
-        let marker = new mapboxgl.Marker().setLngLat(lngLat).addTo(map.current!);
-    });
-
     return (
-        <div>
-            <div style={{ position: "relative" }}>
-                <div className="sidebar">
-                    Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-                </div>
-            </div>
-            <div ref={mapContainer} className="map-container" />
+        <div style={{borderRadius: 5, overflow: "hidden"}}>
+            <Map
+                mapboxAccessToken={mapboxAccessToken}
+                initialViewState={{
+                    longitude: lng,
+                    latitude: lat,
+                    zoom:zoom 
+                }}
+                style={{ width: "auto", height: 400 }}
+                mapStyle="mapbox://styles/mapbox/streets-v12"
+            />
         </div>
     );
 }
