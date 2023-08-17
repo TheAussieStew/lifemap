a916368412b65c89596bc0b95488e57048e67a57
conwayying <101533804+conwayying@users.noreply.github.com>
2023-08-17 12:47:27 +1000

    Attempt to fix token generating

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,7 +11,6 @@ import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import { sign } from 'jsonwebtoken';
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -22,23 +21,17 @@ const appSecret = "Q3rV5UXb8C6ostOsYvzYWw8KGWjrDUWtWNHrsyinSULbOQQ1JQzwBllViSYB3
 admin.initializeApp();
 
 export const generateAuthenticationToken = functions.https.onCall(async (data, context) => {
+    var jwt = require('jsonwebtoken');
     // Check if the user is authenticated
     //   if (!context.auth) {
     //     throw new functions.https.HttpsError('unauthenticated', 'You must be authenticated to use this function.');
     //   }
 
-    const customData = {
-        // Use this list to limit the number of documents that can be accessed by this client.
-        // An empty array means no access at all.
-        // Not sending this property means access to all documents.
-        // We are supporting a wildcard at the end of the string (only there).
-    }
-
     // Sign the JWT with the custom claims and your secret
-    const jwt = sign(customData, appSecret);
+    const token = jwt.sign({ }, appSecret);
 
     // Return the token
-    return { jwt };
+    return { token };
 });
 
 export const helloWorld = onRequest((request, response) => {
