5f2377d6664531a06693af85de5b7e813f4fe40e
conwayying <101533804+conwayying@users.noreply.github.com>
2023-09-04 12:52:49 +1000

    Design information architecture of Calculation node

--- src/view/content/CalculationTipTapExtension.tsx
+++ src/view/content/CalculationTipTapExtension.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Node, mergeAttributes, InputRule, wrappingInputRule, markInputRule, Mark } from "@tiptap/core";
 import { NodeViewWrapper, ReactNodeViewRenderer, nodeInputRule, NodeViewContent } from "@tiptap/react";
 import { MathfieldElement, MathfieldOptions } from "mathlive";
+import { Math } from "../content/Math"
 import { BoxedExpression, ComputeEngine } from '@cortex-js/compute-engine';
 
-export const REGEX_BLOCK_MATH_QUESTION_MARKS: RegExp = /\?\?\s+$/; //new RegExp("\?\?\s+$", "i");
-export const REGEX_INLINE_MATH_QUESTION_MARKS: RegExp = /\?(.+)\?/; //new RegExp("\?(.+)\?", "i");
+const REGEX_BLOCK_MATH_DOLLARS: RegExp = /\$\$(.*?)\$\$/;
+
 
 export const CalculationExtension = Mark.create({
   name: "calculation",
@@ -26,10 +27,33 @@ export const CalculationExtension = Mark.create({
   renderHTML({ HTMLAttributes }) {
     return ["calculation", mergeAttributes(this.options.HTMLAttributes, HTMLAttributes), 0];
   },
+  addAttributes() {
+    // Return an object with attribute configuration
+    return {
+      lensDisplay1: {
+        default: 'natural'
+      },
+      lensEvaluation1: {
+        default: 'evaluate'
+      },
+      equationValue1: {
+        default: ''
+      },
+      lensDisplay2: {
+        default: 'natural'
+      },
+      lensEvaluation2: {
+        default: 'evaluate'
+      },
+      equationValue2: {
+        default: ''
+      }
+    }
+  },
   addInputRules() {
     return [
       markInputRule({
-        find: REGEX_INLINE_MATH_QUESTION_MARKS,
+        find: REGEX_BLOCK_MATH_DOLLARS,
         type: this.type,
         getAttributes: ({ groups }) => groups,
       }),
@@ -44,8 +68,33 @@ export const CalculationExtension = Mark.create({
   },
   addNodeView() {
     return ReactNodeViewRenderer((props: any) => {
+      const updateContent1 = (changedEquation: string) => {
+        props.updateAttributes({ equationValue1: changedEquation });
+        console.log("updated attributes", props.node.attrs)
+      }
+      const updateContent2 = (changedEquation: string) => {
+        props.updateAttributes({ equationValue2: changedEquation });
+        console.log("updated attributes", props.node.attrs)
+      }
+
       return (
         <NodeViewWrapper>
+          <div style={{padding: "5px"}}>
+            <Math
+              style={"flat"}
+              equationString={props.node.attrs.equationValue}
+              lensEvaluation={props.node.attrs.lensEvaluation}
+              lensDisplay={props.node.attrs.lensDisplay}
+              updateContent={updateContent1}
+            />
+            <Math
+              style={"flat"}
+              equationString={props.node.attrs.equationValue}
+              lensEvaluation={props.node.attrs.lensEvaluation}
+              lensDisplay={props.node.attrs.lensDisplay}
+              updateContent={updateContent2}
+            />
+          </div>
         </NodeViewWrapper>
       );
     });
