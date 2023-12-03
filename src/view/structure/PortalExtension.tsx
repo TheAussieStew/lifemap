ded003695a1d8838c085111bba8bdf8262fe04b6
conwayying <101533804+conwayying@users.noreply.github.com>
2023-12-03 14:32:45 +1100

    Get transclusion fully working, plus add portal like styling

--- src/view/structure/PortalExtension.tsx
+++ src/view/structure/PortalExtension.tsx
@@ -5,40 +5,47 @@ import { Editor as CoreEditor } from '@tiptap/core'
 import React, { useEffect, useState } from 'react'
 import { Attrs, Node as ProseMirrorNode } from 'prosemirror-model'
 import { QuantaId } from '../../core/Model'
-import { CustomisedEditor } from '../content/RichText'
+import { MainEditor, TransclusionEditor } from '../content/RichText'
 import { debounce } from 'lodash'
 
 const REGEX_BLOCK_TILDE = /~[^~]+~/
 
+const sharedBorderRadius = 15
+
 // TODO: Ideally, there shouldn't be a CoreEditor and Editor, it should be the same time. Mismatch should not be happening
 const Portal = (props: { editor: CoreEditor, referencedQuantaId: QuantaId }) => {
-    const [editor, setEditor] = React.useState(CustomisedEditor("Content has not been updated to match the referenced node.", true))
-    console.log("editor", editor)
+    const [transclusionEditor, setEditor] = React.useState(TransclusionEditor("Content has not been updated to match the referenced node.", true, true))
 
-
-    useEffect(() => {
-        const updateContent = () => {
-            if (!editor) return
+    const updateContent = () => {
+        if (transclusionEditor) {
+            console.log("portal has editor")
 
             let referencedNode: ProseMirrorNode | undefined = undefined;
 
+            // Check all the nodes of the parent editor to see which one matches
             props.editor.state.doc.descendants(descendant => {
+                // A match was found
                 if (descendant.attrs.quantaId === props.referencedQuantaId) {
                     referencedNode = descendant
                 }
             })
 
+            // No match was found
             if (!referencedNode) {
-                editor.commands.setContent("Couldn't find referenced quanta. Are you sure the id you're using is a valid one?")
+                transclusionEditor.commands.setContent("Couldn't find referenced quanta. Are you sure the id you're using is a valid one?")
             }
+            // A match was found
             else {
-                console.log("referenced node", referencedNode)
+                // Copy the referencedNode
                 const nodeContent = (referencedNode as ProseMirrorNode).toJSON()
-                console.log("node content", nodeContent)
-                editor.commands.setContent(nodeContent)
+                transclusionEditor.commands.setContent(nodeContent)
             }
+        } else {
+            console.error("TransclusionEditor in Portal was unable to initialise.")
         }
+    }
 
+    useEffect(() => {
         // Update the content initially
         updateContent()
 
@@ -46,10 +53,20 @@ const Portal = (props: { editor: CoreEditor, referencedQuantaId: QuantaId }) => 
         const debouncedUpdateContent = debounce(updateContent, 2000)
         props.editor.on('update', debouncedUpdateContent)
 
-    }, [props, editor])
+    }, [props.editor, props.referencedQuantaId])
 
     return (
-        <EditorContent editor={editor} style={{ border: `2px dashed grey`, borderRadius: 5, minHeight: 20, padding: 10 }} />
+        <div style={{
+            borderRadius: sharedBorderRadius,
+            background: `#e0e0e0`,
+            boxShadow: `inset 10px 10px 10px #bebebe,
+            inset -10px -10px 10px #FFFFFF99`,
+            minHeight: 20,
+            padding: `11px 15px 11px 15px`,
+            marginBottom: 10
+        }}>
+            <EditorContent editor={transclusionEditor} />
+        </div>
     )
 }
 
@@ -61,7 +78,7 @@ export const PortalExtension = Node.create({
     addAttributes() {
         return {
             referencedQuantaId: {
-                default: null,
+                default: undefined,
             },
         }
     },
@@ -89,12 +106,20 @@ export const PortalExtension = Node.create({
     },
     addNodeView() {
         return ReactNodeViewRenderer((props: NodeViewProps) => {
+            // On node instantiation, useState will draw from the node attributes
+            // If the attributes are updated, this will re-render, therefore this state is always synced with the node attributes
+            const [referencedQuantaId, setReferencedQuantaId] = useState(props.node.attrs.referencedQuantaId);
+
+            // If the input is updated, this handler is called
+            const handleReferencedQuantaIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+                setReferencedQuantaId(event.target.value);
+                props.updateAttributes({ referencedQuantaId: event.target.value });
+            };
+
             return (
                 <NodeViewWrapper>
-                    <div style={{ border: `1.5px solid grey`, borderRadius: 4, padding: 10, marginBottom: 2, width: "100px", height: "10px", overflow: "hidden", fontSize: 12, flexWrap: "nowrap" }}>
-                        <NodeViewContent />
-                    </div>
-                    <Portal editor={props.editor} referencedQuantaId={props.node.textContent} />
+                    <input type="text" value={referencedQuantaId} onChange={handleReferencedQuantaIdChange} style={{ border: '1.5px solid #34343430', borderRadius: sharedBorderRadius, outline: 'none', backgroundColor: 'transparent', width: `80px`, position: "absolute", zIndex: 1 }} />
+                    <Portal editor={props.editor} referencedQuantaId={referencedQuantaId} />
                 </NodeViewWrapper>
             );
         });
