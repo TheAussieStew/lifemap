e07ff8f738ea878aebe1220949dedaad610f494d
conwayying <101533804+conwayying@users.noreply.github.com>
2023-12-17 14:18:23 +1100

    Animate glow transitions and add important tag glow

--- src/view/structure/GroupTipTapExtension.tsx
+++ src/view/structure/GroupTipTapExtension.tsx
@@ -4,6 +4,7 @@ import { NodeViewContent, NodeViewWrapper, ReactNodeViewRenderer, nodeInputRule 
 import { Group } from "./Group";
 import './styles.scss';
 import { orange } from "@mui/material/colors";
+import { motion } from "framer-motion";
 
 // TODO: Match for brackets with text in between
 export const groupInputRegex = /{([^{}]*)}/;
@@ -38,49 +39,52 @@ export const GroupExtension = Node.create({
   addNodeView() {
     return ReactNodeViewRenderer((props: NodeViewProps) => {
       let node = props.node
-      let containsCompleteMention = false;
       let glowStyles: string[] = []
-      const orangeGlow = `0 0 5px #ff7f00, 0 0 20px #ff7f00, 0 0 30px #ff7f00, 0 0 40px #ff7f00`
-      const greenGlow = `0 0 5px #33cc00, 0 0 10px #33cc00, 0 0 15px #33cc00, 0 0 20px #33cc00`
+      const orangeGlow = `0 0 5px #ff7f0030, 0 0 20px #ff7f0030, 0 0 30px #ff7f0030, 0 0 40px #ff7f0030`
+      const greenGlow = `0 0 5px #33cc0040, 0 0 10px #33cc0040, 0 0 15px #33cc0040, 0 0 20px #33cc0040`
+      const yellowGlow = `0 0 5px #ffec80, 0 0 10px #fff1a3, 0 0 15px #ffed87, 0 0 20px #ffeb7a`
 
       // Check whether this group contains subnodes that is a mention
       node.descendants((childNode) => {
         if (childNode.type.name === 'mention' && (childNode.attrs.label as string).includes('✅ complete')) {
-          containsCompleteMention = true;
+          glowStyles.push(greenGlow)
+        }
+        if (childNode.type.name === 'mention' && (childNode.attrs.label as string).includes('⭐️ important')) {
+          glowStyles.push(yellowGlow)
         }
       });
 
-      if (containsCompleteMention) {
-        glowStyles.push(greenGlow)
-      } else {
-        console.log("The node does not contain a mention node.");
-      }
-
       // Assume 'node' is the specific node you want to check
       let containsUncheckedTodo = false;
+      let containsCheckItem = false;
 
       // Check whether this group contains subnodes that is an incomplete task
       node.descendants((childNode) => {
+        if (childNode.type.name === 'taskItem') {
+          containsCheckItem = true
+        }
         if (childNode.type.name === 'taskItem' && !childNode.attrs.checked) {
           containsUncheckedTodo = true;
         }
       });
 
       if (containsUncheckedTodo) {
-        console.log("The node contains an unchecked to-do list item.");
         glowStyles.push(orangeGlow)
       } else {
-        console.log("The node does not contain an unchecked to-do list item.");
+        // Basically if there's no check items then no glow should appear
+        if (containsCheckItem) {
+          glowStyles.push(greenGlow)
+        }
       }
 
 
       return (
         <NodeViewWrapper>
-          <div style={{borderRadius: 10, boxShadow: glowStyles.join(',')}}>
+          <motion.div style={{ borderRadius: 10 }} animate={{ boxShadow: glowStyles.join(',') }} transition={{ duration: 0.5, ease: "circOut" }}>
             <Group lens={"verticalArray"} quantaId={props.node.attrs.qid}>
               <NodeViewContent />
             </Group>
-          </div>
+          </motion.div>
         </NodeViewWrapper>
       );
     });
