42a20b73a7affcec9fa69a7035cbe4a0ab70d116
conwayying <101533804+conwayying@users.noreply.github.com>
2023-12-17 13:10:38 +1100

    Get additive glows working

--- src/view/structure/GroupTipTapExtension.tsx
+++ src/view/structure/GroupTipTapExtension.tsx
@@ -3,6 +3,7 @@ import { Node, NodeViewProps, mergeAttributes, wrappingInputRule } from "@tiptap
 import { NodeViewContent, NodeViewWrapper, ReactNodeViewRenderer, nodeInputRule } from "@tiptap/react";
 import { Group } from "./Group";
 import './styles.scss';
+import { orange } from "@mui/material/colors";
 
 // TODO: Match for brackets with text in between
 export const groupInputRegex = /{([^{}]*)}/;
@@ -38,8 +39,11 @@ export const GroupExtension = Node.create({
     return ReactNodeViewRenderer((props: NodeViewProps) => {
       let node = props.node
       let containsCompleteMention = false;
-      let classes = ''
+      let glowStyles: string[] = []
+      const orangeGlow = `0 0 5px #ff7f00, 0 0 20px #ff7f00, 0 0 30px #ff7f00, 0 0 40px #ff7f00`
+      const greenGlow = `0 0 5px #33cc00, 0 0 10px #33cc00, 0 0 15px #33cc00, 0 0 20px #33cc00`
 
+      // Check whether this group contains subnodes that is a mention
       node.descendants((childNode) => {
         if (childNode.type.name === 'mention' && (childNode.attrs.label as string).includes('✅ complete')) {
           containsCompleteMention = true;
@@ -47,14 +51,32 @@ export const GroupExtension = Node.create({
       });
 
       if (containsCompleteMention) {
-        classes = 'green-glow'
+        glowStyles.push(greenGlow)
       } else {
         console.log("The node does not contain a mention node.");
       }
 
+      // Assume 'node' is the specific node you want to check
+      let containsUncheckedTodo = false;
+
+      // Check whether this group contains subnodes that is an incomplete task
+      node.descendants((childNode) => {
+        if (childNode.type.name === 'taskItem' && !childNode.attrs.checked) {
+          containsUncheckedTodo = true;
+        }
+      });
+
+      if (containsUncheckedTodo) {
+        console.log("The node contains an unchecked to-do list item.");
+        glowStyles.push(orangeGlow)
+      } else {
+        console.log("The node does not contain an unchecked to-do list item.");
+      }
+
+
       return (
         <NodeViewWrapper>
-          <div className={classes} style={{borderRadius: 10}}>
+          <div style={{borderRadius: 10, boxShadow: glowStyles.join(',')}}>
             <Group lens={"verticalArray"} quantaId={props.node.attrs.qid}>
               <NodeViewContent />
             </Group>
