02eb580b426e0b86351900a69829f62d97b66958
conwayying <101533804+conwayying@users.noreply.github.com>
2023-12-19 17:39:45 +1100

    Revert "Change casing of utils to Utils to fix Vercel deployment error"
    This reverts commit 5d401c7203b5faf65ca568b69eb44334d660a0fc.

--- src/agents/Finesse.ts
+++ src/agents/Finesse.ts
@@ -2,7 +2,7 @@ import { Plugin, PluginKey, TextSelection } from 'prosemirror-state';
 import { Extension } from '@tiptap/core';
 import { openai } from './AI';
 import { highlightGreen, purple } from '../view/Theme';
-import { generatePrompt } from '../utils/Utils';
+import { generatePrompt } from '../utils/utils';
 
 // The Finesse agent scans every added word to see if it's related to emotions.
 export const Finesse = Extension.create({
