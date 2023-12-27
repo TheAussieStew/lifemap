1f6645db381a3034fd420bd0c2fee8389c5e8f52
conwayying <101533804+conwayying@users.noreply.github.com>
2023-12-27 13:17:03 +1100

    Revert "Remove unnecessary App router page"
    This reverts commit 38a8bc9ce2863b4517125a0a14e0b1f0d9ac2847.

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { motion } from 'framer-motion';
+import * as React from 'react';
+import { Quanta } from './core/Quanta';
+import '@fontsource/public-sans';
+
+const App = () => {
+  return (
+    <React.StrictMode>
+      <motion.div style={{ margin: 10, padding: `8px 8px 8px 8px` }}>
+        <Quanta quantaId={'000000'} userId={'000000'} />
+      </motion.div>
+    </React.StrictMode>
+  );
+};
+
+export default App;
