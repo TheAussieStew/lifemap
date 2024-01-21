864985764ae14fb393665f9c8a560affb0dd7236
conwayying <101533804+conwayying@users.noreply.github.com>
2024-01-21 15:29:28 +1100

    Remove delay from minimap

--- app/q/[slug]/page.tsx
+++ app/q/[slug]/page.tsx
@@ -9,10 +9,10 @@ import { Minimap } from "../../../src/view/structure/Minimap";
 export default function Page({ params }: { params: { slug: string } }) {
     return (
         <>
+            <Minimap />
             <motion.div style={{ padding: `40px 100px 40px 100px`, backgroundColor: offWhite }}>
                 <Quanta quantaId={params.slug} userId={'000000'} />
             </motion.div>
-            <Minimap />
         </>
     )
 }
