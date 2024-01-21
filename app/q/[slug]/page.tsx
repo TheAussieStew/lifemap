e9b3734fccbaf3a0df5c3a438d565086d3b62be6
conwayying <101533804+conwayying@users.noreply.github.com>
2024-01-21 15:10:30 +1100

    Move minimap to relative position

--- app/q/[slug]/page.tsx
+++ app/q/[slug]/page.tsx
@@ -4,11 +4,17 @@ import React from "react";
 import { motion } from 'framer-motion';
 import { Quanta } from "../../../src/core/Quanta";
 import { offWhite } from "../../../src/view/Theme";
+import { Minimap } from "../../../src/view/structure/Minimap";
 
 export default function Page({ params }: { params: { slug: string } }) {
     return (
-        <motion.div style={{ padding: `40px`, backgroundColor: offWhite }}>
-            <Quanta quantaId={params.slug} userId={'000000'} />
-        </motion.div>
+        <>
+            <motion.div style={{ display: "relative", width: 65 }}>
+                <Minimap />
+            </motion.div>
+            <motion.div style={{ padding: `40px 100px 40px 100px`, backgroundColor: offWhite }}>
+                <Quanta quantaId={params.slug} userId={'000000'} />
+            </motion.div>
+        </>
     )
 }
