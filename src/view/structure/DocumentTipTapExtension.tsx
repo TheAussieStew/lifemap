8f0db0a15f74137eb9d3d5ae3fd8ee4f3f75f137
conwayying <101533804+conwayying@users.noreply.github.com>
2024-02-26 10:23:37 +1100

    Create focus mode functionality [work in progress, currently the attributes don't exist on the document node]

--- src/view/structure/DocumentTipTapExtension.tsx
+++ src/view/structure/DocumentTipTapExtension.tsx
@@ -1,13 +1,14 @@
 import { Extension } from '@tiptap/core';
 
-const DocumentExtension = Extension.create({
-    name: 'document',
+export const defaultDocumentAttributeValues = {
+    selectedFocusLens: { default: "editing" },
+    selectedHideIrrelevantEventsNodes: { default: 'wedding' },
+}
 
+export const DocumentExtension = Extension.create({
+    name: 'document',
     addAttributes() {
         // This works the same as the node level attributes that contain selected lens information, but this time it's at the document level
-        return {
-            selectedFocusLens: { default: "clear" },
-            selectedHideIrrelevantEventsNodes: { default: 'wedding' },
-        };
+        return defaultDocumentAttributeValues;
     },
 });
