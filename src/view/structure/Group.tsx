485b2f04416bdbb9b5b20147b297b9a575eea7c2
conwayying <101533804+conwayying@users.noreply.github.com>
2024-02-25 18:21:37 +1100

    Abstract out the grip into a dedicated component

--- src/view/structure/Group.tsx
+++ src/view/structure/Group.tsx
@@ -3,6 +3,7 @@ import { Quanta } from '../../core/Quanta';
 import { QuantaId } from '../../core/Model';
 import React from 'react'
 import { offWhite, purple } from '../Theme';
+import { Grip } from '../content/Grip';
 
 export type GroupLenses = "verticalArray";
 
@@ -42,23 +43,7 @@ export const Group = (props: { children: any, lens: GroupLenses, quantaId: Quant
                 margin: `10px 0px 10px 0px`,
             }}
         >
-            <motion.div data-drag-handle
-                onMouseLeave={(event) => {
-                    event.currentTarget.style.cursor = "grab";
-                }}
-                onMouseDown={(event) => {
-                    event.currentTarget.style.cursor = "grabbing";
-                }}
-                onMouseUp={(event) => {
-                    event.currentTarget.style.cursor = "grab";
-                }}
-                style={{ position: "absolute", right: 0, top: 10, display: "flex", flexDirection: "column", cursor: "grab", fontSize: "24px", color: "grey", scale: 1.5 }}
-                contentEditable="false"
-                suppressContentEditableWarning={true}
-                initial={{ opacity: 0 }}
-                whileHover={{ opacity: 1 }}>
-                ⠿
-            </motion.div>
+            <Grip/>
             {props.children}
         </motion.div>
     )
