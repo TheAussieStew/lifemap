6d36f1267b2d5d039e9b2b550a121f55fb318d10
conwayying <101533804+conwayying@users.noreply.github.com>
2024-04-08 10:46:54 +1000

    Create stub for temporal estimates

---
+++ src/view/structure/TemporalEstimate.tsx
@@ -0,0 +1,6 @@
+// I'm looking to create a TemporalEstimate timer based off the PomodoroTimer
+// Instead of the flow switches at the beginning, it simply shows the length of the single audio file that it accepts
+// It also shows a tag that displays the name of the audio file
+// When the play button is pressed, the audio will start playing and a pomodoro will be created with the same length as the audio.
+// If a file is dragged onto the temporal estimate, then that file will become the basis for the audio file
+// Also suggest the exact hosting service that the file is hosted on
