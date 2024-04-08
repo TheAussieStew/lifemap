0dbb1ccc1ae2bf551c0ecabbf420837cb48c0511
conwayying <101533804+conwayying@users.noreply.github.com>
2024-04-08 18:21:06 +1000

    Remove type errors

--- src/view/structure/Calendar.tsx
+++ src/view/structure/Calendar.tsx
@@ -1,20 +1,20 @@
-import Calendar from '@toast-ui/react-calendar';
-import React from 'react';
-import 'tui-calendar/dist/tui-calendar.css';
+// import Calendar from '@toast-ui/react-calendar';
+// import React from 'react';
+// import 'tui-calendar/dist/tui-calendar.css';
 
-export const SchedulingCalendar = () => {
-    const calRef = React.useRef();
+// export const SchedulingCalendar = () => {
+//     const calRef = React.useRef();
 
-    const handleClick = () => {
-        const inst = calRef.current.getInstance();
-        console.log(inst.getDate());
-    };
+//     const handleClick = () => {
+//         const inst = calRef.current.getInstance();
+//         console.log(inst.getDate());
+//     };
 
-    return (
-        <>
-            <button onClick={handleClick}>Test</button>
-            <Calendar ref={calRef} />
-        </>
-    );
+//     return (
+//         <>
+//             <button onClick={handleClick}>Test</button>
+//             <Calendar ref={calRef} />
+//         </>
+//     );
 
-}
+// }
