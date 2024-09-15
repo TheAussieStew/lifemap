643ba37ede91ca7b53cbc10f424d72ae906e1cb0
conwayying <101533804+conwayying@users.noreply.github.com>
2024-09-15 16:24:10 +1000

    Add textured paper to backgrounds

--- app/q/[slug]/page.tsx
+++ app/q/[slug]/page.tsx
@@ -6,19 +6,22 @@ import { Quanta } from "../../../src/core/Quanta";
 import { offWhite } from "../../../src/subapps/Theme";
 import { Minimap } from "../../../src/subapps/controls/Minimap";
 import { DocumentFlowMenu } from "../../../src/subapps/controls/FlowMenu";
-import { Editor } from "@tiptap/core";
 import { MainEditor } from "../../../src/subapps/logos/RichText";
 
 export default function Page({ params }: { params: { slug: string } }) {
     return (
-        <>
+        <div style={{
+            backgroundColor: offWhite,
+            backgroundImage: 'url("https://www.transparenttextures.com/patterns/paper.png")',
+            minHeight: '100vh',
+        }}>
             <Minimap />
             <motion.div style={{display: "grid", placeItems: "center", paddingTop: 15, paddingBottom: 4}}>
                 <DocumentFlowMenu editor={MainEditor("", true)!}/>
             </motion.div>
-            <motion.div style={{ padding: `0px 0px 40px 0px`, backgroundColor: offWhite }}>
+            <motion.div style={{ padding: `0px 0px 40px 0px` }}>
                 <Quanta quantaId={params.slug} userId={'000000'} />
             </motion.div>
-        </>
+        </div>
     )
 }
