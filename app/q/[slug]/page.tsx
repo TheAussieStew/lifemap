7662bbe5c92ccd45d151cbc42a128e6d3f685ed6
conwayying <101533804+conwayying@users.noreply.github.com>
2024-09-24 09:49:58 +1000

    Added pre-alpha indicator

--- app/q/[slug]/page.tsx
+++ app/q/[slug]/page.tsx
@@ -7,10 +7,11 @@ import { offWhite } from "../../../src/subapps/Theme";
 import { Minimap, useMinimapWidth } from "../../../src/subapps/controls/Minimap";
 import { DocumentFlowMenu } from "../../../src/subapps/controls/FlowMenu";
 import { MainEditor } from "../../../src/subapps/logos/RichText";
+import { version } from "../../../package.json";
 
 export default function Page({ params }: { params: { slug: string } }) {
     const minimapWidth = useMinimapWidth();
-    const paddingBuffer = 20; // Add a buffer of 20 pixels
+    const paddingBuffer = 20;
 
     return (
         <div style={{
@@ -27,6 +28,21 @@ export default function Page({ params }: { params: { slug: string } }) {
             <motion.div style={{ padding: `0px 0px 40px 0px` }}>
                 <Quanta quantaId={params.slug} userId={'000000'} />
             </motion.div>
+            <div
+                title="Versioning indicator"
+                style={{
+                    position: 'fixed',
+                    bottom: '10px',
+                    left: '10px', // Changed from 'right' to 'left'
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                    color: 'white',
+                    padding: '5px 10px',
+                    borderRadius: '5px',
+                    fontSize: '13px',
+                    zIndex: 1000,
+                }}>
+                Pre-Alpha v{version}
+            </div>
         </div>
     )
 }
