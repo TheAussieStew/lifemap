5cba7936558b29622334d57328aca08366316519
conwayying <101533804+conwayying@users.noreply.github.com>
2024-09-24 12:28:33 +1000

    Initial change to create a border drag handle. Currently, can't drag, and middle of drag ring captures input. In future, replace this with 4 border handles that overlap forming a square.

--- src/subapps/logos/Group.tsx
+++ src/subapps/logos/Group.tsx
@@ -3,6 +3,9 @@ import { Quanta } from '../../core/Quanta';
 import { QuantaId } from '../../core/Model';
 import React, { useMemo } from 'react'
 import { absoluteWhite } from '../Theme';
+import { Editor } from '@tiptap/core';
+import { MainEditor } from './RichText';
+import { DragRing } from '../controls/DragRing';
 
 export type GroupLenses = "verticalArray";
 
@@ -22,7 +25,7 @@ function hashCode(str: string): number {
     return hash;
 }
 
-export const Group = (props: { children: any, lens: GroupLenses, quantaId?: QuantaId }) => {
+export const Group = (props: { children: any, lens: GroupLenses, quantaId?: QuantaId, editor: Editor }) => {
     const backgroundTexture = useMemo(() => {
         if (!props.quantaId) {
             return textures[Math.floor(Math.random() * textures.length)];
@@ -31,65 +34,55 @@ export const Group = (props: { children: any, lens: GroupLenses, quantaId?: Quan
         return textures[index];
     }, [props.quantaId]);
 
-    // TODO: Exit animation doesn't work
-    // TODO: Fix stretchy border: https://github.com/framer/motion/issues/1249
     return (
         <motion.div
             key="group"
             layoutId={props.quantaId}
-            className="group"
-            initial={{
-                opacity: 0,
-            }}
-            animate={{ opacity: 1 }}
-            exit={{
-                opacity: 0,
-            }}
-            transition={{
-                type: "ease",
-                duration: 0.4,
-                delay: Math.random() / 2
-            }}
-            style={{
-                position: "relative",
-                backgroundColor: absoluteWhite,
-                backgroundImage: backgroundTexture,
-                minHeight: 20,
-                overflow: "hidden",
-                // width: "fit-content",
-                borderRadius: `10px`,
-                // border: `2px solid var(--Light_Grey, #dddddd)`,
-                boxShadow: `0px 0.6021873017743928px 2.0474368260329356px -1px rgba(0, 0, 0, 0.29), 0px 2.288533303243457px 7.781013231027754px -2px rgba(0, 0, 0, 0.27711), 0px 10px 34px -3px rgba(0, 0, 0, 0.2)`,
-                padding: `35px`,
-                margin: `10px 0px 10px 0px`,
-            }}
+            className="group-container"
         >
-            <motion.div data-drag-handle
-                onMouseLeave={(event) => {
-                    event.currentTarget.style.cursor = "grab";
+            {/* Reusable Ring Drag Handle */}
+            <DragRing editor={props.editor} />
+
+            {/* Main Group Content */}
+            <motion.div
+                className="group"
+                initial={{
+                    opacity: 0,
                 }}
-                onMouseDown={(event) => {
-                    event.currentTarget.style.cursor = "grabbing";
+                animate={{ opacity: 1 }}
+                exit={{
+                    opacity: 0,
                 }}
-                onMouseUp={(event) => {
-                    event.currentTarget.style.cursor = "grab";
+                transition={{
+                    type: "ease",
+                    duration: 0.4,
+                    delay: Math.random() / 2
                 }}
-                style={{ position: "absolute", right: -5, top: 10, display: "flex", flexDirection: "column", cursor: "grab", fontSize: "24px", color: "grey" }}
-                contentEditable="false"
-                suppressContentEditableWarning={true}
-                initial={{ opacity: 0 }}
-                whileHover={{ opacity: 1 }}>
-                ⠿
+                style={{
+                    backgroundColor: absoluteWhite,
+                    backgroundImage: backgroundTexture,
+                    minHeight: 20,
+                    overflow: "hidden",
+                    borderRadius: `10px`,
+                    boxShadow: `0px 0.6021873017743928px 2.0474368260329356px -1px rgba(0, 0, 0, 0.29), 
+                                0px 2.288533303243457px 7.781013231027754px -2px rgba(0, 0, 0, 0.27711), 
+                                0px 10px 34px -3px rgba(0, 0, 0, 0.2)`,
+                    padding: `35px`,
+                }}
+            >
+                {props.children}
             </motion.div>
-            {props.children}
         </motion.div>
     )
 }
 
-export const GroupExample = () => {
-    return (
-        <Group lens={"verticalArray"} quantaId={"000001"}>
-            <Quanta quantaId={'000001'} userId={''} />
-        </Group>
-    )
-}
+// export const GroupExample = () => {
+//     // Initialize or retrieve your TipTap editor instance
+//     const editor = MainEditor(null, true);
+
+//     return (
+//         <Group lens={"verticalArray"} quantaId={"000001"} editor={editor}>
+//             <Quanta quantaId={'000001'} userId={''} />
+//         </Group>
+//     )
+// }
