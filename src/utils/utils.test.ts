1e14d93f85e9fbff549bd74868793ee25c194ae9
conwayying <101533804+conwayying@users.noreply.github.com>
2024-12-05 15:31:29 +1100

    Clean up utils tests

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,13 +1,5 @@
 import { expect, test } from 'vitest'
-import { isActualUrl, isWordEmotionRelated, similarityBetweenWordEmbeddings } from './utils'
-
-test('that commonsense embeddings are valid', () => {
-
-    // Words not relating to emotion are identified
-    expect(similarityBetweenWordEmbeddings("black", "white")).toBe(0)
-    expect(similarityBetweenWordEmbeddings("healed", "sealed")).toBe(0.8)
-    expect(similarityBetweenWordEmbeddings("healed", "sealed")).toBe(0.8)
-})
+import { isActualUrl } from './utils'
 
 test('that urls and non-urls are correctly identified', () => {
 
